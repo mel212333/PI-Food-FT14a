f2b27f6c6de19a74037601a4b2aa425198857be1
mel212333 <melina212333@gmail.com>
2021-07-09 05:21:06 -0300

    post

--- api/index.js
+++ api/index.js
@@ -4,41 +4,41 @@ const { conn , Type} = require('./src/db.js');
 
   
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
-  //   var dietKetogenic = Type.create({
-  //     name: "Ketogenic",
-  //   });
+    var dietKetogenic = Type.create({
+      name: "Ketogenic",
+    });
 
-  //   var dietGlutenFree = Type.create({
-  //     name: "Gluten Free",
-  //   });
+    var dietGlutenFree = Type.create({
+      name: "Gluten Free",
+    });
 
-  //   var dietVegetarian = Type.create({
-  //     name: "Vegetarian",
-  //   });
+    var dietVegetarian = Type.create({
+      name: "Vegetarian",
+    });
  
-  //   var dietLactoVegetarian = Type.create({
-  //     name: "Lacto Vegetarian",
-  //   });
+    var dietLactoVegetarian = Type.create({
+      name: "Lacto Vegetarian",
+    });
  
-  //   var dietVegan = Type.create({
-  //     name: "Vegan",
-  //   });
+    var dietVegan = Type.create({
+      name: "Vegan",
+    });
 
-  //   var dietPescetarian = Type.create({
-  //     name: "Pescetarian",
-  //   });
-  //   var dietPaleo = Type.create({
-  //     name: "Paleo",
-  //   });
-  //   var dietPrimal = Type.create({
-  //     name: "Primal",
-  //   });
+    var dietPescetarian = Type.create({
+      name: "Pescetarian",
+    });
+    var dietPaleo = Type.create({
+      name: "Paleo",
+    });
+    var dietPrimal = Type.create({
+      name: "Primal",
+    });
  
-  //   var dietWhole30 = Type.create({
-  //     name: "Whole30",
-  //   });
-   });
+    var dietWhole30 = Type.create({
+      name: "Whole30",
+    });
+  });
 });
